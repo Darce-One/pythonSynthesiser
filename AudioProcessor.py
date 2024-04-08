2934f9f5e13129ab9f2f3d388dbb20ff042e6e4b
Andreas Papaeracleous <andreas@Andreas-Beast.local>
2024-04-08 17:50:10 +0100

    Made the AudioProcessor class more templaty

--- AudioProcessor.py
+++ AudioProcessor.py
@@ -1,15 +1,19 @@
-from typing import List
-from Generators import SinGen
 import numpy as np
+from typing import List
+from Generators import SinGen, SawGen
+from Filters import TwoPoleFilter
+from Envelopes import Ar, Adsr
+
 
 class AudioProcessor():
     def __init__(self, sample_rate: int) -> None:
         self.sample_rate: int = sample_rate
 
     def prepare_to_play(self) -> None:
-        self.sin_gens = [SinGen(440, 0.1, self.sample_rate), SinGen(441.5, 0.1, self.sample_rate)]
+        # Code goes in here...
+        pass
 
     def process_block(self, buffer: np.ndarray) -> None:
-        for i in range(buffer.shape[0]):
+        for n in range(buffer.shape[0]):
             for c in range(buffer.shape[1]):
-                buffer[i,c] += self.sin_gens[c].process()
+                buffer[n, c] += 0.0
