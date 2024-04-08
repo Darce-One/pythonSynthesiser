da110807a0b346cf90f8c7721acee851852a7fd8
Andreas Papaeracleous <andreas@Andreas-Beast.local>
2024-04-08 13:49:34 +0100

    Generators update

--- Generators.py
+++ Generators.py
@@ -1,9 +1,8 @@
 from typing import List
-import numpy as np
 import math
 
 class Phasor():
-    def __init__(self, frequency: float, sample_rate: int) -> None:
+    def __init__(self, sample_rate: int, frequency: float) -> None:
         self.frequency: float = frequency
         self.sample_rate: int = sample_rate
         self.phase: float = 0.0
@@ -17,6 +16,11 @@ class Phasor():
     def _update_phase_delta(self) -> None:
         self.phase_delta = self.frequency / self.sample_rate
 
+    def _update_phase(self) -> None:
+        self.phase += self.phase_delta
+        if self.phase > 1.0:
+            self.phase -= 1.0
+
 
     def set_gain(self, new_gain) -> None:
         self.gain = new_gain
@@ -24,21 +28,27 @@ class Phasor():
 
     def process(self) -> float:
         sample = self.phase * self.gain
-        self.phase += self.phase_delta
-        if self.phase > 1.0:
-            self.phase -= 1.0
+        self._update_phase()
         return sample
 
 
 
 class SinGen(Phasor):
-    def __init__(self, frequency: float, gain: float, sample_rate: int) -> None:
-        super().__init__(frequency, sample_rate)
+    def __init__(self, sample_rate: int, frequency: float, gain: float = 1.0) -> None:
+        super().__init__(sample_rate, frequency)
         self.gain = gain
 
     def process(self) -> float:
-        sample = math.sin(self.phase * 2 * np.pi) * self.gain
-        self.phase += self.phase_delta
-        if self.phase > 1.0:
-            self.phase -= 1.0
+        sample = math.sin(self.phase * 2 * math.pi) * self.gain
+        self._update_phase()
+        return sample
+
+class SawGen(Phasor):
+    def __init__(self, sample_rate: int, frequency: float, gain: float = 1.0) -> None:
+        super().__init__(sample_rate, frequency)
+        self.gain = gain
+
+    def process(self) -> float:
+        sample = (2 * self.phase - 1) * self.gain
+        self._update_phase()
         return sample
